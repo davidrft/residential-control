a91d376abc41001c252315dd86f754a8587404eb
Diego Hamilton <diegomhamilton@github.com>
2018-12-17 12:19:31 -0300

    Set buttons and images Layout it is not done.

--- gui/residentialControl.py
+++ gui/residentialControl.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (QWidget, QToolTip,
     QPushButton, QApplication, QDesktopWidget,
     QLabel, QLCDNumber, QGridLayout, QLineEdit)
 from PyQt5.QtGui import (QIcon, QFont, QColor)
+from PyQt5.QtCore import (QSize)
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -19,32 +20,49 @@ class MainWindow(QWidget):
         panicButton.resize(panicButton.sizeHint())
         panicButton.clicked.connect(self.panicButtonClicked)
 
-        tempLabel = QLabel('Temperatura')
-        lumLabels = [QLabel('Luminosidade 1'), QLabel('Luminosidade 2')]
+        modeSwitch = QPushButton('MANUAL', self)
+        modeSwitch.resize(modeSwitch.sizeHint())
+        modeSwitch.clicked.connect(self.modeSwitchClicked)
+        self.modeSwitchState = True
+
+        # tempLabel = QLabel('Temperatura')
+        lumLabels = [QLabel('Luz Interna'), QLabel('Luz Externa')]
         lightSwitchLabel = QLabel('Luz Interna')
         lightSwitch2Label = QLabel('Luz Externa')
-        gateSwitchLabel = QLabel('Abrir/Fechar Portão')
+        # gateSwitchLabel = QLabel('Portão')
         serialInLabel = QLabel('Último dado recebido')
         serialOutLabel = QLabel('Último dado enviado')
 
         self.tempLCD = QLCDNumber()
         self.setTempThreshold = QLineEdit()
-        # self.setTempThreshold.textChanged.connect()
-        self.lumLCD = [QLCDNumber(), QLCDNumber()]
+        # self.lumLCD = [QLCDNumber(), QLCDNumber()]
+
+        airCondSwitch = QPushButton('Temperatura', self)
+        airCondSwitch.resize(airCondSwitch.sizeHint())
+        airCondSwitch.clicked.connect(self.airCondSwitchClicked)
+        airCondSwitch.setIcon(QIcon('imgs/airoff.png'))
+        airCondSwitch.setIconSize(QSize(51,51))
+        self.airCondSwitchState = False
         
-        lightSwitch = QPushButton("Luz 1 OFF", self)
+        lightSwitch = QPushButton('', self)
         lightSwitch.resize(lightSwitch.sizeHint())
         lightSwitch.clicked.connect(self.lightSwitchClicked)
+        lightSwitch.setIcon(QIcon('imgs/luzoff.png'))
+        lightSwitch.setIconSize(QSize(51,51))
         self.lightSwitchState = False
 
-        lightSwitch2 = QPushButton("Luz 2 OFF", self)
+        lightSwitch2 = QPushButton('', self)
         lightSwitch2.resize(lightSwitch2.sizeHint())
         lightSwitch2.clicked.connect(self.lightSwitch2Clicked)
+        lightSwitch2.setIcon(QIcon('imgs/luzoff.png'))
+        lightSwitch2.setIconSize(QSize(51,51))
         self.lightSwitch2State = False
 
-        gateSwitch = QPushButton("Abrir", self)
+        gateSwitch = QPushButton('Portão Fechado', self)
         gateSwitch.resize(gateSwitch.sizeHint())
         gateSwitch.clicked.connect(self.gateSwitchClicked)
+        gateSwitch.setIcon(QIcon('imgs/gateclosed.png'))
+        gateSwitch.setIconSize(QSize(46,32))
         self.gateSwitchState = False
 
         self.serialIn = QLineEdit(self)
@@ -54,23 +72,24 @@ class MainWindow(QWidget):
         grid = QGridLayout()
         grid.setSpacing(10)
 
-        grid.addWidget(tempLabel, 1, 0)
+        # grid.addWidget(tempLabel, 1, 0)
+        grid.addWidget(airCondSwitch, 1, 0)
         grid.addWidget(self.tempLCD, 1, 1)
-        grid.addWidget(QLabel("Set Threshold"), 1, 2)
+        grid.addWidget(QLabel('Limite de Temperatura'), 1, 2)
         grid.addWidget(self.setTempThreshold, 1, 3)
         
-        grid.addWidget(lumLabels[0], 2, 0)
-        grid.addWidget(self.lumLCD[0], 2, 1)
+        # grid.addWidget(lumLabels[0], 2, 0)
+        # grid.addWidget(self.lumLCD[0], 2, 1)
 
-        grid.addWidget(lumLabels[1], 3, 0)
-        grid.addWidget(self.lumLCD[1], 3, 1)
+        # grid.addWidget(lumLabels[1], 3, 0)
+        # grid.addWidget(self.lumLCD[1], 3, 1)
 
         grid.addWidget(lightSwitchLabel, 2, 2)
         grid.addWidget(lightSwitch, 3, 2)
         grid.addWidget(lightSwitch2Label, 2, 3)
         grid.addWidget(lightSwitch2, 3, 3)
 
-        grid.addWidget(gateSwitchLabel, 4, 2)
+        # grid.addWidget(gateSwitchLabel, 4, 2)
         grid.addWidget(gateSwitch, 4, 3)
 
         grid.addWidget(panicButton, 5, 1)
@@ -93,26 +112,59 @@ class MainWindow(QWidget):
     def panicButtonClicked(self):
         print(f'{self.sender().text()} was pressed')
         return
+        
+    def modeSwitchClicked(self):
+        print(f'{self.sender().text()} was pressed')
+        self.sender().setText('AUTO' if self.modeSwitchState else 'MANUAL')
+        self.modeSwitchState = not self.modeSwitchState
+        self.sender().setChecked(False)
+        return
 
     def lightSwitchClicked(self):
         print(f'{self.sender().text()} was pressed')
+        if self.modeSwitchState or self.commandReceived:
+            icon = QIcon('imgs/luzoff.png' if self.lightSwitchState else 'imgs/luzon.png')
+            self.sender().setIcon(icon)
+            # self.sender().setText('Luz 1 OFF' if self.lightSwitchState else 'Luz 1 ON')
+            self.lightSwitchState = not self.lightSwitchState
+            self.commandReceived = False
+        else:
+            return
 
-        self.sender().setText("Luz 1 OFF" if self.lightSwitchState else "Luz 1 ON")
-        self.lightSwitchState = not self.lightSwitchState
+    def airCondSwitchClicked(self):
+        print(f'{self.sender().text()} was pressed')
+        if self.modeSwitchState or self.commandReceived:
+            icon = QIcon('imgs/airoff.png' if self.airCondSwitchState else 'imgs/airon.png')
+            self.sender().setIcon(icon)
+            # self.sender().setText('Luz 1 OFF' if self.lightSwitchState else 'Luz 1 ON')
+            self.airCondSwitchState = not self.airCondSwitchState
+            self.commandReceived = False
+        else:
+            return
 
     def lightSwitch2Clicked(self):
         print(f'{self.sender().text()} was pressed')
+        if self.modeSwitchState or self.commandReceived:
+            icon = QIcon('imgs/luzoff.png' if self.lightSwitch2State else 'imgs/luzon.png')
+            self.sender().setIcon(icon)
+            # self.sender().setText('Luz 2 OFF' if self.lightSwitch2State else 'Luz 2 ON')
+            self.lightSwitch2State = not self.lightSwitch2State
+            self.commandReceived = False
+        else:
+            return
 
-        self.sender().setText("Luz 2 OFF" if self.lightSwitch2State else "Luz 2 ON")
-        self.lightSwitch2State = not self.lightSwitch2State
-    
     def gateSwitchClicked(self):
         print(f'{self.sender().text()} was pressed')
+        if self.modeSwitchState or self.commandReceived:
+            icon = QIcon('imgs/gateclosed.png' if self.gateSwitchState else 'imgs/gateopen.png')
+            self.sender().setText('Portão Fechado' if self.gateSwitchState else 'Portão Aberto')
+            self.sender().setIcon(icon)
+            self.gateSwitchState = not self.gateSwitchState
+            self.sender().setChecked(False)
+            self.commandReceived = False
+        else:
+            return
 
-        self.sender().setText("Abrir" if self.gateSwitchState else "Fechar")
-        self.gateSwitchState = not self.gateSwitchState
-        self.sender().setChecked(False)
-    
     def luminosityChanged(idx, self):
         return
 
@@ -120,7 +172,7 @@ class MainWindow(QWidget):
         try:
             tempThreshold = int(self.setTempThreshold.text())
         except:
-            print("!!!! WARNING !!!! Invalid temperature threshold")
+            print('!!!! WARNING !!!! Invalid temperature threshold')
             tempThreshold = self.tempLCD.value()
 
         if self.tempLCD.value() <= tempThreshold:
@@ -153,9 +205,9 @@ class MainWindow(QWidget):
         palette.setColor(palette.WindowText, color)
         # background color, default: white
         palette.setColor(palette.Background, QColor(0, 0, 0))
-        # "light" border
+        # 'light' border
         palette.setColor(palette.Light, color)
-        # "dark" border
+        # 'dark' border
         palette.setColor(palette.Dark, color)
         lcd.setPalette(palette)
 
@@ -173,8 +225,8 @@ if __name__ == '__main__':
 
     # USB_PORT = 'USB0'
     # com = serial.Serial(f'/dev/tty{USB_PORT}', 115200)
-    number = input("Type a number: ")
+    number = input('Type a number: ')
     ex.tempLCD.display(number)
-    ex.newDataReceived("C")
+    ex.newDataReceived('C')
     ex.temperatureChanged()
     sys.exit(app.exec_())
